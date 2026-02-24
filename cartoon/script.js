02b40cba64e694b2cff002a130c06e48719fed73
m-kabir02 <sxdiyak@gmail.com>
2026-02-24 00:24:34 -0500

    added back link

--- cartoon/script.js
+++ cartoon/script.js
@@ -79,68 +79,97 @@ window.addEventListener('resize', function () {
     ctx.fill();
     ctx.restore();
 
+    // Drawing the caption text on the canvas
+    ctx.fillStyle = 'black';
+    ctx.font = '30px Arial';
+    ctx.fillText('Welcome to My Home', 35, 50);
+
+// Drawing the caption text on the canvas
+    ctx.fillStyle = 'black';
+    ctx.font = '20px Arial';
+    ctx.fillText('Use your mouse to add grass!', 35, 100);
+
 })
 
     //Drawing the sun
+    ctx.fillStyle = 'yellow';
+    ctx.beginPath();
+    ctx.arc(430, 100, 50, 0, Math.PI * 2);
+    ctx.fill();
+    
+    //Drawing the house
+    ctx.save();
+    ctx.translate(20, 450)
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.fillRect(0, 0, 450, 350);
+    ctx.fill();
+    ctx.restore();
 
-ctx.fillStyle = 'yellow';
-ctx.beginPath();
-ctx.arc(430, 100, 50, 0, Math.PI * 2);
-ctx.fill();
+    //The Roof
+    ctx.save(); // Bookmark the current state (origin at 0,0)
+    ctx.translate(245, 225);
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.rotate(45* Math.PI / 180);
+    ctx.fillRect(0, 0, 318, 318);
+    ctx.fill();
+    ctx.restore();
 
-//Drawing the house
-ctx.save(); // Bookmark the current state (origin at 0,0)
-ctx.translate(100, 450);
-ctx.fillStyle = 'red';
-ctx.beginPath();
-ctx.fillRect(0, 0, 450, 300);
-ctx.fill();
-ctx.restore();
+  //Drawing the left window
+    ctx.save(); // Bookmark the current state (origin at 0,0)
+    ctx.translate(50, 475);
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.fillRect(0, 0, 100, 100);
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
 
-//Drawing the roof
-ctx.save(); // Bookmark the current state (origin at 0,0)
-ctx.translate(325, 225);
-ctx.fillStyle = 'red';
-ctx.beginPath();
-ctx.rotate(45* Math.PI / 180);
-ctx.fillRect(0, 0, 318, 318);
-ctx.fill();
-ctx.restore();
+    //Drawing the right window
+    ctx.save(); // Bookmark the current state (origin at 0,0)
+    ctx.translate(330, 475);
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.fillRect(0, 0, 100, 100);
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
 
-//Drawing the Door
-ctx.save(); // Bookmark the current state (origin at 0,0)
-ctx.translate(250, 580);
-ctx.fillStyle = 'brown';
-ctx.beginPath();
-ctx.fillRect(0, 0, 125, 175);
-ctx.fill();
-ctx.restore();
+    //Drawing the Door
+    ctx.save(); // Bookmark the current state (origin at 0,0)
+    ctx.translate(180, 580);
+    ctx.fillStyle = 'brown';
+    ctx.beginPath();
+    ctx.fillRect(0, 0, 125, 175);
+    ctx.fill();
+    ctx.restore();
+    
+    //Doorknob
+    ctx.fillStyle = 'gold';
+    ctx.beginPath();
+    ctx.arc(290, 690, 10, 0, Math.PI * 2);
+    ctx.fill();
 
-//Drawing the left window
-ctx.save(); // Bookmark the current state (origin at 0,0)
-ctx.translate(130, 475);
-ctx.fillStyle = 'white';
-ctx.beginPath();
-ctx.fillRect(0, 0, 100, 100);
-ctx.stroke();
-ctx.fill();
-ctx.restore();
+    //The Grass
+    ctx.save();
+    ctx.fillStyle = '#004400';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.fillRect(0, 750, canvas.width, 40);
+    ctx.fill();
+    ctx.restore();
 
-//Drawing the right window
-ctx.save(); // Bookmark the current state (origin at 0,0)
-ctx.translate(400, 475);
-ctx.fillStyle = 'white';
-ctx.beginPath();
-ctx.fillRect(0, 0, 100, 100);
-ctx.stroke();
-ctx.fill();
-ctx.restore();
+    // Drawing the caption text on the canvas
+    ctx.fillStyle = 'black';
+    ctx.font = '30px Arial';
+    ctx.fillText('Welcome to My Home', 35, 50);
 
-//Doorknob
-ctx.fillStyle = 'gold';
-ctx.beginPath();
-ctx.arc(360, 690, 10, 0, Math.PI * 2);
-ctx.fill();
+// Drawing the caption text on the canvas
+    ctx.fillStyle = 'black';
+    ctx.font = '20px Arial';
+    ctx.fillText('Use your mouse to add grass!', 35, 100);
+
 
 
 const mouse = {
@@ -183,15 +212,6 @@ function drawGrass() {
 
 */
 
-//Horizontal green block for grass field.
-ctx.fillStyle = '#004400';
-ctx.strokeStyle = '#004400';
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.fillRect(0, 750, canvas.width, 40);
-ctx.stroke();
-ctx.fill();
-
 //Draws grass when mouse clicked
 function drawGrass() {
     for (let i = 0; i < canvas.width; i += 5) {
@@ -218,6 +238,16 @@ function drawGrass() {
         ctx.stroke();
     }
 }
+
+// Drawing the caption text on the canvas
+ctx.fillStyle = 'black';
+ctx.font = '30px Arial';
+ctx.fillText('Welcome to My Home', 35, 50);
+
+// Drawing the caption text on the canvas
+ctx.fillStyle = 'black';
+ctx.font = '20px Arial';
+ctx.fillText('Use your mouse to add grass!', 35, 100);
 
 
 
